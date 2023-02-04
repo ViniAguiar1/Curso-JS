301f68f2d17742eade674cef3b4eefa30133c5c9
viniaguiar20 <88693741+viniaguiar20@users.noreply.github.com>
2023-02-04 18:07:50 -0300

    Att

--- aula35/assets/js/main.js
+++ aula35/assets/js/main.js
@@ -4,3 +4,16 @@ const elementos = [
     {tag: 'footer', texto: 'Frase 3'},
     {tag: 'section', texto: 'Frase 4'},
 ];
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+
+for (let i = 0; i < elementos.length; i++) {
+    let { tag, texto } = elementos[i];
+    let tagCriada = document.createElement(tag);
+    let textoCriado = document.createTextNode(texto)
+
+    tagCriada.appendChild(textoCriado)
+    div.appendChild(tagCriada);
+}
+
+container.appendChild(div);
