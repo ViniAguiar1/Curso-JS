6559bee9c22cbd997623b10873905fb41d1f327f
viniaguiar20 <88693741+viniaguiar20@users.noreply.github.com>
2023-02-15 14:35:08 -0300

    Att

--- filter-filtrando-arrays/aula65.js
+++ filter-filtrando-arrays/aula65.js
@@ -6,6 +6,15 @@
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 
     15, 22, 27];
 
+    // function callbackFilter(valor){
+       // return valor > 10;
+    // }; 
+
+    const numerosFiltrados = numeros.filter(function(valor){
+        return valor > 10;
+    });
+    console.log(numerosFiltrados);
+
     // retorne as pessoas que tem o nome com 5 letras ou mais
     // retorne as pessoas com mais de 50 anos
     // retorne as pessoas cujo nome termina com a
